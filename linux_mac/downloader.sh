df52307712cd547f1cb81fcfbf7239c86c86b006
Joergi <1439809+joergi@users.noreply.github.com>
2020-07-20 23:27:59 +0200

    Update downloader.sh

--- linux_mac/downloader.sh
+++ linux_mac/downloader.sh
@@ -46,54 +46,6 @@ i=1
 	while [ "$i" -le "$recentIssue" ]
 	do
 		printf -v page_url $downloadUrl "$i"
-		#!/bin/bash
-# ------------------------------------------------------------------
-# [Author] joergi - 
-#          This script is a generic download script for the:
-#          - MagPi Downloader (https://github.com/joergi/MagPiDownloader/)
-#          - HelloWorld Downloader (https://github.com/joergi/HelloWorldDownloader)
-#          - Wireframe Downloader (https://github.com/joergi/WireframeDownloader)
-#          - Hackspace Magazine Downloader (https://github.com/joergi/Hackspace-Magazine-Downloader)
-#          this script is under GNU GENERAL PUBLIC LICENSE
-# ------------------------------------------------------------------
-
-# VERSION=0.2.0
-# $1 = download Url (mandatory)
-# $2 = recent issue (not mandatory, because of the special editions)
-# $3 = -f first issue to download
-# $4 = -l last issue to download
-
-if [[ -z $1 ]]; then
-    echo "download url can not be empty"
-    exit 1
-fi
-
-recentIssue=1
-
-if [[ ! -z $2 ]]; then
-   recentIssue=$2
-fi
-
-downloadUrl=$1
-
-
-i=1
-
-	while :
-	do
-		case "$3" in
-		-f) shift; i="$3";;
-		-l) shift; recentIssue="$3";;
-		--) shift; break;;
-		-*) usage "bad argument $3";;
-		*) break;;
-		esac
-		shift
-	done
-
-	while [ "$i" -le "$recentIssue" ]
-	do
-		printf -v page_url $downloadUrl "$i"
 		pdf_url=$(curl -sf "$page_url" | grep c-link | sed 's/^.*href=\"//' | sed 's/\?.*$//')
 		wget -N "$pdf_url" -P "$OUTDIR"
 		i=$(( i+1 ))
