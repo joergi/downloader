1bd2c645a92dacbbfa49e4eb0da63fc00f5bc010
joergi <1439809+joergi@users.noreply.github.com>
2023-06-02 12:18:36 +0200

    use strict mode
    - complete new way of getting the variables in Linux
    
    #18

--- linux_mac/downloader.sh
+++ linux_mac/downloader.sh
@@ -21,6 +21,7 @@ IFS=$'\n\t'
 
 # for special issues, at the moment the last line must be an empty line, else the last one is ignored
 
+
 if [[ -z $1 ]]; then
     echo "download url can not be empty"
     exit 1
@@ -31,11 +32,38 @@ if [[ -z $2 ]]; then
     exit 1
 fi
 
+
+
 recentIssue=1
 
 if [[ ! -z $3 ]]; then
    recentIssue=$3
 fi
+
+first=-1
+last=-1
+echo "first: $first"
+echo "last: $last"
+
+ if [ $# -ge 5 ] && [ ! -z $4 ]; then
+  if [ $4 == "-f" ]; then
+    first=$5
+  elif [ $4 == "-l" ]; then
+    last=$5
+
+  fi
+ fi
+
+ if [ $# -ge 7 ] &&[ ! -z $6 ]; then
+   if [ "$6" == "-l" ]; then
+     last=$7
+   fi
+ else
+   last=$recentIssue
+ fi
+
+echo "first: $first"
+echo "last: $last"
 
 downloadUrl=$1
 outputDir=$2
@@ -43,20 +71,33 @@ outputDir=$2
 siteUrl="$(echo $downloadUrl | sed 's/\(https:\/\/[^\/]*\)\/.*$/\1/')"
 
 i=1
+if [ "$first" != -1 ]; then
+  i=$first
+fi
+if [ "last" != -1 ]; then
+  recentIssue=$last
+fi
 
-	while :
-	do
-		case "$4" in
-		-f) shift; i="$4";;
-		-l) shift; recentIssue="$4";;
-		--) shift; break;;
-		-*) usage "bad argument $4";;
-		*) break;;
-		esac
-		shift
-	done
 
-	while [ "$i" -le "$recentIssue" ]
+#while :
+#do
+#    if [ "$#" -lt 4 ]; then
+#        usage "Insufficient number of parameters."
+#    fi
+#
+#    case "$4" in
+#    -f) shift; i="$4";;
+#    -l) shift; recentIssue="$4";;
+#    --) shift; break;;
+#    -*) usage "bad argument $4";;
+#    *) break;;
+#    esac
+#    shift
+#done
+
+
+	while [ "$i" -le "$last" ]
+	echo "in while - i: $i and last $last"
 	do
     printf -v page_url $downloadUrl "$i"
 
