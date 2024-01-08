f3258171053a871973939ed4ed55fa0ebb89b062
joergi <1439809+joergi@users.noreply.github.com>
2024-01-08 21:24:23 +0100

    everything before flashing

--- windows/downloader.ps1
+++ windows/downloader.ps1
@@ -7,27 +7,45 @@ param (
 )
 
 # Extract the site URL from the download URL
+
+Write-Output "download url $downloadUrl"
 $siteUrl = $downloadUrl -replace "(https://[^\/]+).*",'$1'
+Write-Output $siteUrl
 
 for ($i = $firstIssue; $i -le $recentIssue; $i++) {
     $pageUrl = $downloadUrl -f $i
 
     # c-link download is only for helloworld mag
-    $pdfUrl = Invoke-WebRequest -Uri $pageUrl -UseBasicParsing |
-            Select-String -Pattern '\"c-link\" download=\"([^\"]+)\"' |
-            ForEach-Object { $_.Matches.Groups[1].Value }
+#    $pdfUrl = Invoke-WebRequest -Uri $pageUrl -UseBasicParsing | Select-String  [regex]::Match($htmlSnippet, '<a class="c-link" download="[^"]+" href="([^"]+)"').Groups[1].Value
+#            Select-String -Pattern '\"c-link\" download=\"([^\"]+)\"' |
+#            ForEach-Object { $_.Matches.Groups[1].Value }
+
+#    $pdfUrl = [regex]::Match($htmlSnippet, '<a class="c-link" download="[^"]+" href="([^"]+)"').Groups[1].Value
+    $pdfUrl = [regex]::Match($htmlSnippet, '<a class="c-link" download="([^"]+)" href="([^"]+)"').Groups[2].Value
+
+
+    Write-Output "after clink 1st  $pdfUrl"
+
+    $x = [string]::IsNullOrEmpty($pdfUrl)
+
+    Write-Output "xxxxxxxxxxxx  $x"
 
     if ([string]::IsNullOrEmpty($pdfUrl)) {
+        Write-Output "in if §pdfUrl"
         # Magpi, Wireframe + Hackspace
         $pdfUrl = Invoke-WebRequest -Uri $pageUrl -UseBasicParsing |
                 Select-String -Pattern '\"c-link\" href=\"([^\"]+)\"' |
                 ForEach-Object { $_.Matches.Groups[1].Value }
+        Write-Output "after if $pdfUrl"
+
     }
 
     if ([string]::IsNullOrEmpty($pdfUrl)) {
         Write-Host "PDF URL not found for issue $i"
         continue
     }
+
+    Write-Output "$pdfUrl"
 
     $pdfUrl = $pdfUrl -replace '\?.*',''
     $pdfUrl = $pdfUrl -replace '^/', "$siteUrl/"
